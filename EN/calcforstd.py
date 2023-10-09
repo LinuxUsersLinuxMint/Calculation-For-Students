cd89c69d00ac51c4c24c88f439dfedad9193e0d3
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-09 23:02:00 +0300

    Updated calcforstd.py v1.0.2

--- EN/calcforstd.py
+++ EN/calcforstd.py
@@ -5,22 +5,41 @@
 "Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 "A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 
-okulturu=str(input('Okul Türünüzü Giriniz (Ortaokul/Lise): '))
+okulturu=str(input('Enter The School Type: (Middle School/High School): '))
 islem=str(input('Yapmak İstediğiniz İşlemi Seçiniz: '))
 
-if islem=="yazılıort":
-    yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
-    yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
-    ortsonuc=yz1+yz2/2
-    print("Yazılı Ortalamanız: {0}". format(ortsonuc))
-elif islem=="ortsnf":
-    yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
-    if yzort >= 50:
+if okulturu=="Middle School":
+    if islem=="yazılıort":
+        yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
+        yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
+        ortsonuc=yz1+yz2/2
+        print("Yazılı Ortalamanız: {0}". format(ortsonuc))
+    elif islem=="ortsnf":
+        yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
+    elif yzort >= 50:
         print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
     else:
         print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
         print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
-elif islem=="about":
-    print("Calculation For Students v1.0 (Son Güncellemne tarihi 9 , Eylül , 2023 , 17:33)")
-else:
-    print("Geçersiz İşlem veya işlem girilmedi...")
+    elif islem=="about":
+        print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
+    else:
+        print("Invalid Process...!")
+
+elif okulturu=="High School":
+    if islem=="yazılıort":
+        yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
+        yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
+        ortsonuc=yz1+yz2/2
+        print("Yazılı Ortalamanız: {0}". format(ortsonuc))
+    elif islem=="ortsnf":
+        yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
+    elif yzort >= 50:
+        print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
+    else:
+        print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
+        print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
+    elif islem=="about":
+        print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
+    else:
+        print("Invalid Process...!)
--- TR/calcforstd.py
+++ TR/calcforstd.py
@@ -8,19 +8,38 @@
 okulturu=str(input('Okul Türünüzü Giriniz (Ortaokul/Lise): '))
 islem=str(input('Yapmak İstediğiniz İşlemi Seçiniz: '))
 
-if islem=="yazılıort":
-    yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
-    yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
-    ortsonuc=yz1+yz2/2
-    print("Yazılı Ortalamanız: {0}". format(ortsonuc))
-elif islem=="ortsnf":
-    yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
-    if yzort >= 50:
+if okulturu=="Ortaokul":
+    if islem=="yazılıort":
+        yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
+        yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
+        ortsonuc=yz1+yz2/2
+        print("Yazılı Ortalamanız: {0}". format(ortsonuc))
+    elif islem=="ortsnf":
+        yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
+    elif yzort >= 50:
         print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
     else:
         print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
         print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
-elif islem=="about":
-    print("Calculation For Students v1.0 (Son Güncellemne tarihi 9 , Eylül , 2023 , 17:33)")
-else:
-    print("Geçersiz İşlem veya işlem girilmedi...")
+    elif islem=="about":
+        print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
+    else:
+        print("Geçersiz İşlem veya işlem girilmedi...")
+
+elif okulturu=="Lise":
+    if islem=="yazılıort":
+        yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
+        yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
+        ortsonuc=yz1+yz2/2
+        print("Yazılı Ortalamanız: {0}". format(ortsonuc))
+    elif islem=="ortsnf":
+        yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
+    elif yzort >= 50:
+        print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
+    else:
+        print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
+        print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
+    elif islem=="about":
+        print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
+    else:
+        print("Geçersiz İşlem veya işlem girilmedi...")
