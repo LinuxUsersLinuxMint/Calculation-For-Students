f145b3be0bf749c2d0bc5fef976eb612d76a045e
LinuxUsersLinuxMint <umutarda863@gmail.com>
2023-10-11 20:14:26 +0300

    Initial commit

--- EN/calcforstd.py
+++ EN/calcforstd.py
@@ -5,41 +5,41 @@
 "Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 "A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
 
-okulturu=str(input('Enter The School Type: (Middle School/High School): '))
-islem=str(input('Yapmak İstediğiniz İşlemi Seçiniz: '))
+schooltype=str(input('Enter The School Type: (Middle School/High School): '))
+process=str(input('Yapmak İstediğiniz İşlemi Seçiniz: '))
 
-if okulturu=="Middle School":
-    if islem=="yazılıort":
+if schooltype=="Middle School":
+    if process=="yazılıort":
         yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
         yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
         ortsonuc=yz1+yz2/2
         print("Yazılı Ortalamanız: {0}". format(ortsonuc))
-    elif islem=="ortsnf":
+    elif process=="ortsnf":
         yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
     elif yzort >= 50:
         print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
     else:
         print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
         print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
-    elif islem=="about":
+    elif process=="about":
         print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
     else:
         print("Invalid Process...!")
 
-elif okulturu=="High School":
-    if islem=="yazılıort":
+elif schooltype=="High School":
+    if process=="yazılıort":
         yz1=float(input('1.Yazılı Notunuzu Giriniz: '))
         yz2=float(input('2.Yazılı Notunuzu Giriniz: '))
         ortsonuc=yz1+yz2/2
         print("Yazılı Ortalamanız: {0}". format(ortsonuc))
-    elif islem=="ortsnf":
+    elif process=="ortsnf":
         yzort=float(input('Yazılı Ortalamanızı Giriniz: '))
     elif yzort >= 50:
         print("Yazılı Ortalamanız 50'den Yüksek Sınıfı Geçtiniz")
     else:
         print("Üzgünüm Yazılı Ortalamanız 50'den Düşük Sınıfı Geçemediniz...")
         print("Ancak Üzülmeyin Her Zaman Daha iyisini Yapabilirsiniz.\n")
-    elif islem=="about":
+    elif process=="about":
         print("Calculation For Students v1.0.2 (Son Güncellemne tarihi 10 , Ekim , 2023 , 22:58)")
     else:
         print("Invalid Process...!)
